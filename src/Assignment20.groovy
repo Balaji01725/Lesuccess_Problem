3888ab2dc3c59cfea8e7e5bc23b9f83415d93321
ADMIN <balajimaharajan017@gmail.com>
2025-06-12 08:04:05 +0530

    commit

---
+++ src/Assignment20.groovy
@@ -0,0 +1,72 @@
+//def s="Groovy Programming";
+//int count=0;
+//for(int i=0;i<s.length();i++){
+//    if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+//        count++;
+//    }
+//}
+//print(count);
+
+//reverse
+//def s="KnowKode"
+//println(s.reverse());
+
+//isPrime
+//def n=17;
+//if(n<=1) return false;
+//
+//for(int i=2;i<=Math.sqrt(n);i++){
+//    if(n%i==0){
+//       return false;
+//    }
+//    return true;
+//}
+
+//Remove Duplicates
+//def list=[1, 2, 2, 3, 4, 4, 5];
+//println list.unique();
+
+//Find common elements in two lists
+//def list1 = [1, 2, 3, 4]
+//def list2 = [3, 4, 5, 6]
+//
+//def common=list1.intersect (list2);
+//println common
+
+
+//Check if two strings are anagrams
+//def isAnagram(String s1,String s2){
+//    if(s1.length()!=s2.length()) return false;
+//    s1.toList().sort()==s2.toList().sort();
+//}
+//
+//def res=isAnagram("listen","silent");
+//println res;
+
+
+//Print Fibonacci series up to N terms
+
+//def printFibonacci(int n) {
+//    def a = 0, b = 1
+//    print("$a ")
+//
+//    if (n > 1) {
+//        print("$b ")
+//    }
+//
+//    for (int i = 3; i <= n; i++) {
+//        def next = a + b
+//        print("$next ")
+//        a = b
+//        b = next
+//    }
+//}
+
+
+//Check if a string is a palindrome
+//
+//def s="madam";
+//print s==s.reverse();
+
+
+
